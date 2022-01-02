251c703a384de433c69f2c446ed274b632ebe9a4
Rick Martin <rmartin1985@att.net>
2022-01-02 10:42:52 -0600

    me query added to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,22 @@ const { Book, User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
-const resolvers = {};
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password');
+
+                    return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+    Mutation: {
+
+    }
+};
 
 module.exports = resolvers;
