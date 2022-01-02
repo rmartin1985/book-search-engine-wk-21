202db333498590692d8a9d8a3047a45b18a8c1c7
Rick Martin <rmartin1985@att.net>
2022-01-02 12:23:39 -0600

    ADD_USER mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,16 @@ mutation login($email: String!, $password: String!) {
     }
 }
 `;
+
+export const ADD_USER = gql`
+mutaation addUser($username: String!, email: String!, password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+            email
+        }
+    }
+}
+`;
