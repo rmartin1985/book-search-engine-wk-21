a268f1e69d1353903f6d8768392977e1283c9e6e
Rick Martin <rmartin1985@att.net>
2022-01-02 13:34:05 -0600

    updated to LOGIN_USER mutation

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,10 +2,17 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+// removed the loginUser from API
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
+  // varible for LOGIN_USER mutation
+  const [loginUser] = useMutation(LOGIN_USER);
+
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,7 +33,9 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await loginUser({
+        variables: { ...userFormData }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -4,7 +4,7 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
-import { createUser } from '../utils/API';
+// removed the createUser from API
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
